f4eab7612d4d29c0235390f917433943b79eea6e
zsdgvsg12 <sunweitongjobs@163.com>
2026-06-23 17:15:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import yfinance as yf
 
-spy = yf.download("SPY", period="5d")
+vix = yf.download("^VIX", period="5d")["Close"].iloc[-1]
+spy = yf.download("SPY", period="1y")["Close"]
 
-print("DATAFRAME:")
-print(spy)
+ma50 = spy.rolling(50).mean().iloc[-1]
+ma200 = spy.rolling(200).mean().iloc[-1]
 
-print("COLUMNS:", spy.columns)
-print("SHAPE:", spy.shape)
+print("VIX:", vix)
+print("MA50:", ma50)
+print("MA200:", ma200)
+
+if vix > 30:
+    print("🚨 VIX > 30")
+
+if ma50 > ma200:
+    print("📈 Golden Cross")
+else:
+    print("📉 Death Cross")
